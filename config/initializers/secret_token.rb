f1c6ed74ff554d7d0349f49a68c60ee0889f8c99
drtoast <toast@drtoast.com>
2011-06-04 13:08:24 -0700

    sort tracks by track number

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Toastunes::Application.config.secret_token = 'cd2143bb0d9b3a01a30ed13aac80e1a4b0f9232b08cc2e96e982d925f726d096da93627241d25b350b1ac3963f86b7cd45b6d279e2c055d8d53272deed92b0a6'
+Toastunes::Application.config.secret_token = 'xcd2143bb0d9b3a01a30ed13aac80e1a4b0f9232b08cc2e96e982d925f726d096da93627241d25b350b1ac3963f86b7cd45b6d279e2c055d8d53272deed92b0a6'
